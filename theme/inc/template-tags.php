ea0f81d36ac060d551b99d4bb0bec3387a7c7e26
scott@kahunam.com <scott@kahunam.com>
2026-03-30 09:49:30 +0100

    refactor: remove kahu_content_class, replace sr-only with screen-reader-text in template tags

--- theme/inc/template-tags.php
+++ theme/inc/template-tags.php
@@ -2,9 +2,7 @@
 /**
  * Custom template tags for this theme
  *
- * Eventually, some functionality here could be replaced by core features.
- *
- * @package _test
+ * @package kahu
  */
 
 if ( ! function_exists( 'kahu_posted_on' ) ) :
@@ -39,8 +37,7 @@ if ( ! function_exists( 'kahu_posted_by' ) ) :
 	 */
 	function kahu_posted_by() {
 		printf(
-		/* translators: 1: posted by label, only visible to screen readers. 2: author link. 3: post author. */
-			'<span class="sr-only">%1$s</span><span class="author vcard"><a class="url fn n" href="%2$s">%3$s</a></span>',
+			'<span class="screen-reader-text">%1$s</span><span class="author vcard"><a class="url fn n" href="%2$s">%3$s</a></span>',
 			esc_html__( 'Posted by', 'kahu' ),
 			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
 			esc_html( get_the_author() )
@@ -54,8 +51,7 @@ if ( ! function_exists( 'kahu_comment_count' ) ) :
 	 */
 	function kahu_comment_count() {
 		if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-			/* translators: %s: Name of current post. Only visible to screen readers. */
-			comments_popup_link( sprintf( __( 'Leave a comment<span class="sr-only"> on %s</span>', 'kahu' ), get_the_title() ) );
+			comments_popup_link( sprintf( __( 'Leave a comment<span class="screen-reader-text"> on %s</span>', 'kahu' ), get_the_title() ) );
 		}
 	}
 endif;
@@ -67,49 +63,38 @@ if ( ! function_exists( 'kahu_entry_meta' ) ) :
 	 */
 	function kahu_entry_meta() {
 
-		// Hide author, post date, category and tag text for pages.
 		if ( 'post' === get_post_type() ) {
 
-			// Posted by.
 			kahu_posted_by();
-
-			// Posted on.
 			kahu_posted_on();
 
-			/* translators: used between list items, there is a space after the comma. */
 			$categories_list = get_the_category_list( __( ', ', 'kahu' ) );
 			if ( $categories_list ) {
 				printf(
-				/* translators: 1: posted in label, only visible to screen readers. 2: list of categories. */
-					'<span><span class="sr-only">%1$s</span>%2$s</span>',
+					'<span><span class="screen-reader-text">%1$s</span>%2$s</span>',
 					esc_html__( 'Posted in', 'kahu' ),
 					$categories_list // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 				);
 			}
 
-			/* translators: used between list items, there is a space after the comma. */
 			$tags_list = get_the_tag_list( '', __( ', ', 'kahu' ) );
 			if ( $tags_list ) {
 				printf(
-				/* translators: 1: tags label, only visible to screen readers. 2: list of tags. */
-					'<span><span class="sr-only">%1$s</span>%2$s</span>',
+					'<span><span class="screen-reader-text">%1$s</span>%2$s</span>',
 					esc_html__( 'Tags:', 'kahu' ),
 					$tags_list // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 				);
 			}
 		}
 
-		// Comment count.
 		if ( ! is_singular() ) {
 			kahu_comment_count();
 		}
 
-		// Edit post link.
 		edit_post_link(
 			sprintf(
 				wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers. */
-					__( 'Edit <span class="sr-only">%s</span>', 'kahu' ),
+					__( 'Edit <span class="screen-reader-text">%s</span>', 'kahu' ),
 					array(
 						'span' => array(
 							'class' => array(),
@@ -128,49 +113,38 @@ if ( ! function_exists( 'kahu_entry_footer' ) ) :
 	 */
 	function kahu_entry_footer() {
 
-		// Hide author, post date, category and tag text for pages.
 		if ( 'post' === get_post_type() ) {
 
-			// Posted by.
 			kahu_posted_by();
-
-			// Posted on.
 			kahu_posted_on();
 
-			/* translators: used between list items, there is a space after the comma. */
 			$categories_list = get_the_category_list( __( ', ', 'kahu' ) );
 			if ( $categories_list ) {
 				printf(
-				/* translators: 1: posted in label, only visible to screen readers. 2: list of categories. */
-					'<span><span class="sr-only">%1$s</span>%2$s</span>',
+					'<span><span class="screen-reader-text">%1$s</span>%2$s</span>',
 					esc_html__( 'Posted in', 'kahu' ),
 					$categories_list // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 				);
 			}
 
-			/* translators: used between list items, there is a space after the comma. */
 			$tags_list = get_the_tag_list( '', __( ', ', 'kahu' ) );
 			if ( $tags_list ) {
 				printf(
-				/* translators: 1: tags label, only visible to screen readers. 2: list of tags. */
-					'<span><span class="sr-only">%1$s</span>%2$s</span>',
+					'<span><span class="screen-reader-text">%1$s</span>%2$s</span>',
 					esc_html__( 'Tags:', 'kahu' ),
 					$tags_list // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 				);
 			}
 		}
 
-		// Comment count.
 		if ( ! is_singular() ) {
 			kahu_comment_count();
 		}
 
-		// Edit post link.
 		edit_post_link(
 			sprintf(
 				wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers. */
-					__( 'Edit <span class="sr-only">%s</span>', 'kahu' ),
+					__( 'Edit <span class="screen-reader-text">%s</span>', 'kahu' ),
 					array(
 						'span' => array(
 							'class' => array(),
@@ -185,8 +159,7 @@ endif;
 
 if ( ! function_exists( 'kahu_post_thumbnail' ) ) :
 	/**
-	 * Displays an optional post thumbnail, wrapping the post thumbnail in an
-	 * anchor element except when viewing a single post.
+	 * Displays an optional post thumbnail.
 	 */
 	function kahu_post_thumbnail() {
 		if ( ! kahu_can_show_post_thumbnail() ) {
@@ -195,23 +168,19 @@ if ( ! function_exists( 'kahu_post_thumbnail' ) ) :
 
 		if ( is_singular() ) :
 			?>
-
 			<figure>
 				<?php the_post_thumbnail(); ?>
-			</figure><!-- .post-thumbnail -->
-
+			</figure>
 			<?php
 		else :
 			?>
-
 			<figure>
 				<a href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
 					<?php the_post_thumbnail(); ?>
 				</a>
 			</figure>
-
 			<?php
-		endif; // End is_singular().
+		endif;
 	}
 endif;
 
@@ -219,11 +188,9 @@ if ( ! function_exists( 'kahu_comment_avatar' ) ) :
 	/**
 	 * Returns the HTML markup to generate a user avatar.
 	 *
-	 * @param mixed $id_or_email The Gravatar to retrieve. Accepts a user_id, gravatar md5 hash,
-	 *                           user email, WP_User object, WP_Post object, or WP_Comment object.
+	 * @param mixed $id_or_email The Gravatar to retrieve.
 	 */
 	function kahu_get_user_avatar_markup( $id_or_email = null ) {
-
 		if ( ! isset( $id_or_email ) ) {
 			$id_or_email = get_current_user_id();
 		}
@@ -265,42 +232,5 @@ if ( ! function_exists( 'kahu_the_posts_navigation' ) ) :
 				'next_text' => __( 'Older posts', 'kahu' ),
 			)
 		);
-	}
-endif;
-
-if ( ! function_exists( 'kahu_content_class' ) ) :
-	/**
-	 * Displays the class names for the post content wrapper.
-	 *
-	 * This allows us to add Tailwind Typography’s modifier classes throughout
-	 * the theme without repeating them in multiple files. (They can be edited
-	 * at the top of the `../functions.php` file via the
-	 * KAHU_TYPOGRAPHY_CLASSES constant.)
-	 *
-	 * Based on WordPress core’s `body_class` and `get_body_class` functions.
-	 *
-	 * @param string|string[] $classes Space-separated string or array of class
-	 *                                 names to add to the class list.
-	 */
-	function kahu_content_class( $classes = '' ) {
-		$all_classes = array( $classes, KAHU_TYPOGRAPHY_CLASSES );
-
-		foreach ( $all_classes as &$class_groups ) {
-			if ( ! empty( $class_groups ) ) {
-				if ( ! is_array( $class_groups ) ) {
-					$class_groups = preg_split( '#\s+#', $class_groups );
-				}
-			} else {
-				// Ensure that we always coerce class to being an array.
-				$class_groups = array();
-			}
-		}
-
-		$combined_classes = array_merge( $all_classes[0], $all_classes[1] );
-		$combined_classes = array_map( 'esc_attr', $combined_classes );
-
-		// Separates class names with a single space, preparing them for the
-		// post content wrapper.
-		echo 'class="' . esc_attr( implode( ' ', $combined_classes ) ) . '"';
 	}
 endif;
