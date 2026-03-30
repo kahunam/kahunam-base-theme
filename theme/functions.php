9a6e685503f11bac9102c9d58d4749f7e1285be2
scott@kahunam.com <scott@kahunam.com>
2026-03-30 09:45:45 +0100

    feat: rewrite functions.php — remove Tailwind, add Verbose Framework enqueues and footer nav menus

--- theme/functions.php
+++ theme/functions.php
@@ -1,94 +1,37 @@
 <?php
 /**
- * _test functions and definitions
+ * Kahunam Base Theme functions and definitions
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package _test
+ * @package kahu
  */
 
 if ( ! defined( 'KAHU_VERSION' ) ) {
-	/*
-	 * Set the theme’s version number.
-	 *
-	 * This is used primarily for cache busting. If you use `npm run bundle`
-	 * to create your production build, the value below will be replaced in the
-	 * generated zip file with a timestamp, converted to base 36.
-	 */
 	define( 'KAHU_VERSION', '0.1.0' );
-}
-
-if ( ! defined( 'KAHU_TYPOGRAPHY_CLASSES' ) ) {
-	/*
-	 * Set Tailwind Typography classes for the front end, block editor and
-	 * classic editor using the constant below.
-	 *
-	 * For the front end, these classes are added by the `kahu_content_class`
-	 * function. You will see that function used everywhere an `entry-content`
-	 * or `page-content` class has been added to a wrapper element.
-	 *
-	 * For the block editor, these classes are converted to a JavaScript array
-	 * and then used by the `./javascript/block-editor.js` file, which adds
-	 * them to the appropriate elements in the block editor (and adds them
-	 * again when they’re removed.)
-	 *
-	 * For the classic editor (and anything using TinyMCE, like Advanced Custom
-	 * Fields), these classes are added to TinyMCE’s body class when it
-	 * initializes.
-	 */
-	define(
-		'KAHU_TYPOGRAPHY_CLASSES',
-		'prose prose-neutral max-w-none prose-a:text-primary'
-	);
 }
 
 if ( ! function_exists( 'kahu_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
 	 */
 	function kahu_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on _test, use a find and replace
-		 * to change 'kahu' to the name of your theme in all the template files.
-		 */
 		load_theme_textdomain( 'kahu', get_template_directory() . '/languages' );
 
-		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
-
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
 		add_theme_support( 'title-tag' );
-
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
 		add_theme_support( 'post-thumbnails' );
 
-		// This theme uses wp_nav_menu() in two locations.
 		register_nav_menus(
 			array(
-				'menu-1' => __( 'Primary', 'kahu' ),
-				'menu-2' => __( 'Footer Menu', 'kahu' ),
+				'primary'  => __( 'Primary', 'kahu' ),
+				'footer-1' => __( 'Footer Col 1', 'kahu' ),
+				'footer-2' => __( 'Footer Col 2', 'kahu' ),
+				'footer-3' => __( 'Footer Col 3', 'kahu' ),
+				'footer-4' => __( 'Footer Col 4', 'kahu' ),
 			)
 		);
 
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
 		add_theme_support(
 			'html5',
 			array(
@@ -102,19 +45,8 @@ if ( ! function_exists( 'kahu_setup' ) ) :
 			)
 		);
 
-		// Add theme support for selective refresh for widgets.
 		add_theme_support( 'customize-selective-refresh-widgets' );
-
-		// Add support for editor styles.
-		add_theme_support( 'editor-styles' );
-
-		// Enqueue editor styles.
-		add_editor_style( 'style-editor.css' );
-
-		// Add support for responsive embedded content.
 		add_theme_support( 'responsive-embeds' );
-
-		// Remove support for block templates.
 		remove_theme_support( 'block-templates' );
 	}
 endif;
@@ -122,8 +54,6 @@ add_action( 'after_setup_theme', 'kahu_setup' );
 
 /**
  * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function kahu_widgets_init() {
 	register_sidebar(
@@ -144,71 +74,16 @@ add_action( 'widgets_init', 'kahu_widgets_init' );
  * Enqueue scripts and styles.
  */
 function kahu_scripts() {
-	wp_enqueue_style( 'kahu-style', get_stylesheet_uri(), array(), KAHU_VERSION );
-	wp_enqueue_script( 'kahu-script', get_template_directory_uri() . '/js/script.min.js', array(), KAHU_VERSION, true );
+	wp_enqueue_style( 'kahu-framework', get_template_directory_uri() . '/css/framework.css', array(), KAHU_VERSION );
+	wp_enqueue_style( 'kahu-theme', get_template_directory_uri() . '/css/theme.css', array( 'kahu-framework' ), KAHU_VERSION );
+	wp_enqueue_style( 'kahu-style', get_stylesheet_uri(), array( 'kahu-theme' ), KAHU_VERSION );
+	wp_enqueue_script( 'kahu-navigation', get_template_directory_uri() . '/js/navigation.js', array(), KAHU_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'kahu_scripts' );
-
-/**
- * Enqueue the block editor script.
- */
-function kahu_enqueue_block_editor_script() {
-	$current_screen = function_exists( 'get_current_screen' ) ? get_current_screen() : null;
-
-	if (
-		$current_screen &&
-		$current_screen->is_block_editor() &&
-		'widgets' !== $current_screen->id
-	) {
-		wp_enqueue_script(
-			'kahu-editor',
-			get_template_directory_uri() . '/js/block-editor.min.js',
-			array(
-				'wp-blocks',
-				'wp-edit-post',
-			),
-			KAHU_VERSION,
-			true
-		);
-		wp_add_inline_script( 'kahu-editor', "tailwindTypographyClasses = '" . esc_attr( KAHU_TYPOGRAPHY_CLASSES ) . "'.split(' ');", 'before' );
-	}
-}
-add_action( 'enqueue_block_assets', 'kahu_enqueue_block_editor_script' );
-
-/**
- * Add the Tailwind Typography classes to TinyMCE.
- *
- * @param array $settings TinyMCE settings.
- * @return array
- */
-function kahu_tinymce_add_class( $settings ) {
-	$settings['body_class'] = KAHU_TYPOGRAPHY_CLASSES;
-	return $settings;
-}
-add_filter( 'tiny_mce_before_init', 'kahu_tinymce_add_class' );
-
-/**
- * Limit the block editor to heading levels supported by Tailwind Typography.
- *
- * @param array  $args Array of arguments for registering a block type.
- * @param string $block_type Block type name including namespace.
- * @return array
- */
-function kahu_modify_heading_levels( $args, $block_type ) {
-	if ( 'core/heading' !== $block_type ) {
-		return $args;
-	}
-
-	// Remove <h1>, <h5> and <h6>.
-	$args['attributes']['levelOptions']['default'] = array( 2, 3, 4 );
-
-	return $args;
-}
-add_filter( 'register_block_type_args', 'kahu_modify_heading_levels', 10, 2 );
 
 /**
  * Custom template tags for this theme.
